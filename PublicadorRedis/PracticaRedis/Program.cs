0eed922690dd40b64eb4b33836addde6b1639ade
ChristopherDeLaRosa <christopherdelarosadiaz@gmail.com>
2024-06-28 21:42:30 -0400

    Entregable

---
+++ PublicadorRedis/PracticaRedis/Program.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+
+//Publicador
+var redis = ConnectionMultiplexer.Connect("redis-15781.c251.east-us-mz.azure.redns.redis-cloud.com:15781, password = GDLMUusHs5R64txnOFJHKuL6TDVSAKYi");
+var pubSub = redis.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Ingresa el mensaje a publicar: ");
+    var message = Console.ReadLine();
+    pubSub.Publish("real_time_channel", message);
+    Console.WriteLine("Mensaje Publicado.");
+}
---
+++ SuscriptorRedis/SuscriptorRedis/Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+
+//Suscriptor
+var redis = ConnectionMultiplexer.Connect("redis-15781.c251.east-us-mz.azure.redns.redis-cloud.com:15781,password=GDLMUusHs5R64txnOFJHKuL6TDVSAKYi");
+var pubSub = redis.GetSubscriber();
+
+pubSub.Subscribe("real_time_channel", (channel, message) =>
+{
+    Console.WriteLine($"Mensaje Recibido: {message}");
+});
+
+Console.WriteLine("Suscrito al canal. Esperando mensajes...");
+Console.ReadLine(); 
+    
